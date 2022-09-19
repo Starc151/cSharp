e4dda7255e75f1e3f22b167308952099aace8336
Aleksey Pashkov <dr.Starc@ya.ru>
2022-09-19 18:08:30 +0300

    zadacha4 + правка zadacha2

--- dz1/Program.cs
+++ dz1/Program.cs
@@ -4,7 +4,6 @@ Clear();
 /* Задача 2: Напишите программу, которая на вход 
 принимает два числа и выдаёт, какое число большее, а 
 какое меньшее. */
-
 void zadacha2(){
     Write("Введите первое число: ");
     int number1 = Convert.ToInt32(ReadLine());
@@ -17,7 +16,7 @@ void zadacha2(){
     }
     else if (number1 < number2)
     {
-        WriteLine("Певрое число {number1} меньше, чем второе число {number2}");
+        WriteLine($"Певрое число {number1} меньше, чем второе число {number2}");
     }
     else
     {
@@ -25,4 +24,27 @@ void zadacha2(){
     }
 }
 
-zadacha2();
+/* Задача 4: Напишите программу, которая принимает на 
+вход три числа и выдаёт максимальное из этих чисел. */
+void zadacha4(){
+    Write("Введите первое число: ");
+    int number1 = Convert.ToInt32(ReadLine());
+    Write("Введите второе число: ");
+    int number2 = Convert.ToInt32(ReadLine());
+    Write("Введите третье число: ");
+    int number3 = Convert.ToInt32(ReadLine());
+    int max = number1;
+
+    if (max < number2)
+    {
+        max = number2;
+    }
+    if (max < number3)
+    {
+        max = number3;
+    }
+    WriteLine($"Максимальное число из введённых: {max}");
+}
+
+// zadacha2();
+zadacha4();
