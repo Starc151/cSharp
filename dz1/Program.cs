85db3de24e596a8b4e9259d8e2a4ebbe67e96d14
Aleksey Pashkov <dr.Starc@ya.ru>
2022-09-19 17:31:19 +0300

    adacha2

--- dz1/Program.cs
+++ dz1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+/* Задача 2: Напишите программу, которая на вход 
+принимает два числа и выдаёт, какое число большее, а 
+какое меньшее. */
+
+void zadacha2(){
+    Write("Введите первое число: ");
+    int number1 = Convert.ToInt32(ReadLine());
+    Write("Введите второе число: ");
+    int number2 = Convert.ToInt32(ReadLine());
+
+    if (number1 > number2)
+    {
+        WriteLine($"Первое число {number1} больше, чем второе число {number2}");
+    }
+    else if (number1 < number2)
+    {
+        WriteLine("Певрое число {number1} меньше, чем второе число {number2}");
+    }
+    else
+    {
+        WriteLine("Числа одинаковы"); 
+    }
+}
+
+zadacha2();
