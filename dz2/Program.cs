65bb420e150d5312a4c9eb8be07156fca9903580
Aleksey Pashkov <dr.Starc@ya.ru>
2022-09-21 20:02:13 +0300

    zadacha13

--- dz2/Program.cs
+++ dz2/Program.cs
@@ -10,5 +10,21 @@ void zadacha10(){
     
 }
 
+/*Напишите программу, которая выводит третью цифру заданного числа
+или сообщает, что третьей цифры нет.*/
+void zadacha13(){
+    Write("Введите число: ");
+    int number = Convert.ToInt32(ReadLine());
+    if (number<100){
+        Write("третьей цифры нет");
+        return;
+    };
+    while(number>1000){
+        number/=10;
+    }
+    Write(number%10);
 
-zadacha10();
+}
+// zadacha10();
+zadacha13();
+
