f890c6488bfa55284b2340c7d42c42655f9637cf
Aleksey Pashkov <dr.Starc@ya.ru>
2022-09-22 22:18:46 +0300

    one triangles

--- triangles/Program.cs
+++ triangles/Program.cs
@@ -1,2 +1,13 @@
 ﻿using static System.Console;
 Clear();
+
+int xa = 40, ya = 1,  //первая точка
+    xb = 1, yb = 30,  //вторая точка
+    xc = 80, yc = 30; //третья точка
+
+SetCursorPosition(xa, xb);
+Write("+");
+SetCursorPosition(xb, yb);
+Write("+");
+SetCursorPosition(xc, yc);
+Write("+");
