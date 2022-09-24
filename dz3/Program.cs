d2a9665324714d3ba140beacc0c451067a8c6290
Aleksey Pashkov <dr.Starc@ya.ru>
2022-09-24 20:02:57 +0300

    zadacha19

--- dz3/Program.cs
+++ dz3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+/*Задача 19 Напишите программу, которая принимает на вход пятизначное число
+и проверяет, является ли оно палиндромом.*/
+void zadacha19(){
+    Write("Введите 5тизначное число: ");
+    int num = Convert.ToInt32(ReadLine());  // ??!!
+    if(num<10000||num>99999){return;};
+    if((num%10==num/10000)&&(num/10%10==num/1000%10)){
+        Write("Да");
+    }
+    else{
+      Write("Нет");  
+    }
+}
+//zadacha19();
