9fc61b39255db0a487f3c0aab3b71c2ebc89baa4
Aleksey Pashkov <dr.Starc@ya.ru>
2022-09-25 19:27:02 +0300

    zadacha21

--- dz3/Program.cs
+++ dz3/Program.cs
@@ -1,11 +1,12 @@
 ﻿using static System.Console;
+using static System.Math;
 Clear();
 
 /*Задача 19 Напишите программу, которая принимает на вход пятизначное число
 и проверяет, является ли оно палиндромом.*/
 void zadacha19(){
     Write("Введите 5тизначное число: ");
-    int num = Convert.ToInt32(ReadLine());  // ??!!
+    int num = Convert.ToInt32(ReadLine());
     if(num<10000||num>99999){return;};
     if((num%10==num/10000)&&(num/10%10==num/1000%10)){
         Write("Да");
@@ -14,4 +15,23 @@ void zadacha19(){
       Write("Нет");  
     }
 }
+
+/*Задача 21 Напишите программу, которая принимает на вход координаты двух точек
+и находит расстояние между ними в 3D пространстве.*/
+// AB = √(b[0] - a[0])2 + (b[1] - a[1])2 + (b[2] - a[2])2
+void zadacha21(){
+  double[] a = {0, 0, 0};
+  double[] b = {0, 0, 0};
+  
+  WriteLine("Введите координаты первой точки через enter: ");
+  for (int i=0; i<3; i++){
+    a[i] = Convert.ToInt32(ReadLine());
+  }
+  WriteLine("Введите координаты второй точки через enter: ");
+  for (int i=0; i<3; i++){
+    b[i] = Convert.ToInt32(ReadLine());
+  }
+  Write(Round(Sqrt(Pow((b[0] - a[0]), 2)+Pow((b[1] - a[1]), 2)+Pow((b[2] - a[2]), 2)), 2));
+}
 //zadacha19();
+zadacha21();
