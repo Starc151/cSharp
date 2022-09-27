7a3970974d1a84abb535fe8ba80e2e2f2aea85b7
Aleksey Pashkov <dr.Starc@ya.ru>
2022-09-27 14:44:23 +0300

    zadacha25

--- dz4/Program.cs
+++ dz4/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+/*Задача 25: Напишите метод, который принимает на вход два числа (A и B)
+и возводит число A в натуральную степень B с использованием цикла.*/
+
+int GetDegree(int num1, int num2){
+    int result=1;
+    for(int i=1; i<=num2; i++){
+        result *= num1;
+    }
+    return result;         // В задаче не указано, что функция должна результат вернуть или вывести
+    // WriteLine(result);
+}
+GetDegree(3, 5);
