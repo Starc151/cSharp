3914faba674786730740ebdb1f6a2b22dbffda78
Aleksey Pashkov <dr.Starc@ya.ru>
2022-09-28 18:54:07 +0300

    MaxDuplicate run

--- MaxDuplicate/Program.cs
+++ MaxDuplicate/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+void MaxDuplicate(int numElements, int numDesired){
+    int count=0, result=0;
+    int[] arr = new int[numElements];
+    for (int i=0; i<numElements; i++){
+        arr[i] = new Random().Next(0, 2);
+        // Write($"{arr[i]}/, ");
+        //
+        if(arr[i]==numDesired){
+            count++;
+        } else {
+            if(result<count){
+                result=count;
+            }
+            count=0;
+        }
+        // выводит элемент массива/счетчик/результат для ручной проверки
+        Write($"{arr[i]}/{count}/{result}, ");
+    }
+    if(result<count){
+        result=count;
+    }
+
+    WriteLine();
+    Write(result);    
+
+}
+
+MaxDuplicate(20, 0);
