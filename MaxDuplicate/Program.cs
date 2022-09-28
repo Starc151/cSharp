5ecb7659cbf585066ecbb8a7918cb7afde0e3634
Aleksey Pashkov <dr.Starc@ya.ru>
2022-09-28 20:41:27 +0300

    universal method

--- MaxDuplicate/Program.cs
+++ MaxDuplicate/Program.cs
@@ -1,12 +1,12 @@
 ﻿using static System.Console;
 Clear();
 
-int[] MaxDuplicate(int numElements, int numSought){
+int[] MaxDuplicate(int numElements, int numSought, int maxElements){
     int count=0;
     int[] result= {0, numElements, numSought};
     int[] arr = new int[numElements];
     for (int i=0; i<numElements; i++){
-        arr[i] = new Random().Next(0, 2);
+        arr[i] = new Random().Next(0, maxElements+1);
         if(arr[i]==numSought){count++;}
         else {
             if(result[0]<count){result[0]=count;}
@@ -15,15 +15,17 @@ int[] MaxDuplicate(int numElements, int numSought){
     }
     //Красивый вывод масива (взято из инета)
     WriteLine("[{0}]", string.Join(", ", arr));
-    
     // конечная проверка на максимальный результат
+    // нужна, если искомая последовательность замыкает массив
     if(result[0]<count){result[0]=count;}
     WriteLine();
     return result;    // Возвращает массив, где result[0] ответ, result[1], длина массива, result[2] искомый элемент
 }
 WriteLine("Введите длину массива");
 int length = Convert.ToInt32(ReadLine());
+WriteLine("Введите значение максимального элемента массива (от 1 до 9)");
+int max = Convert.ToInt32(ReadLine());
 WriteLine("Введите элемент для поиска");
 int sought = Convert.ToInt32(ReadLine());
-int[] maxDuplicate = MaxDuplicate(length, sought);
+int[] maxDuplicate = MaxDuplicate(length, sought, max);
 WriteLine($"В массиве длинной {maxDuplicate[1]}, элемент {maxDuplicate[2]} встречается максимум {maxDuplicate[0]} раз подряд");
