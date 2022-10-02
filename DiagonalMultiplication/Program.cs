da0b939aae6256ec1d7f76201fbdb0a0c62317d4
Aleksey Pashkov <dr.Starc@ya.ru>
2022-10-02 20:46:17 +0300

    honest array

--- DiagonalMultiplication/Program.cs
+++ DiagonalMultiplication/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+int[] DiagonalMultiplication(){
+    int[] arr={1, 2, 3, 4, 5, 6};
+    // определяеи длину получаемого массива
+    int resultLengt=0;
+    if(arr.Length%2==0){
+        resultLengt=arr.Length/2;
+    }else{
+        resultLengt=arr.Length/2+1;
+    }
+    int[] result=new int[resultLengt];
+    
+    for(int i=0; i<arr.Length/2; i++){
+        result[i]=arr[i]*arr[arr.Length-i-1];
+        // if(result[i]==arr[arr.Length-i-1]){
+        //     result[arr[result.Length/2]]=arr[arr.Length/2];
+        //     break;
+        // }
+    }
+    // result[arr[result.Length/2]]=arr[arr.Length/2];
+    return result;
+}
+Write(String.Join(", ", DiagonalMultiplication()));
+
