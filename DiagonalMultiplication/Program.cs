1be59837b3426a56be4e99b1a6760501c726e70b
Aleksey Pashkov <dr.Starc@ya.ru>
2022-10-02 23:55:04 +0300

    full

--- DiagonalMultiplication/Program.cs
+++ DiagonalMultiplication/Program.cs
@@ -2,22 +2,16 @@
 Clear();
 
 int[] DiagonalMultiplication(){
-    int[] arr={1, 2, 3, 4, 5, 6};
+    int[] arr={1, 2, 3, 4, 5};
     // определяеи длину получаемого массива
-    int resultLengt=0;
-    if(arr.Length%2==0){
-        resultLengt=arr.Length/2;
-    }else{
-        resultLengt=arr.Length/2+1;
-    }
-    int[] result=new int[resultLengt];
+    int[] result=new int[arr.Length/2+arr.Length%2];
     
     for(int i=0; i<arr.Length/2; i++){
         result[i]=arr[i]*arr[arr.Length-i-1];
-        // if(result[i]==arr[arr.Length-i-1]){
-        //     result[arr[result.Length/2]]=arr[arr.Length/2];
-        //     break;
-        // }
+        if(arr[i]==arr[arr.Length-1-i]){break;}
+        if(result[i]==arr[arr.Length-i-1]){
+            result[arr[result.Length/2]]=arr[arr.Length/2];
+        }
     }
     // result[arr[result.Length/2]]=arr[arr.Length/2];
     return result;
