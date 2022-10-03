14cf3403bce3a53c70ca32eb1b4ac4b26b18cd6b
AlekseyP <dr.Starc@ya.r>
2022-10-03 17:00:07 +0300

    task34

--- dz5/Program.cs
+++ dz5/Program.cs
@@ -6,3 +6,14 @@ Random rnd=new Random();
 for(int i=0; i<10; i++){
     arr[i]=rnd.Next(99, 1000);
 }
+Write(String.Join(", ", arr));
+WriteLine();
+//Задача 34:Напишите программу, которая покажет количество чётных чисел в массиве.
+void PrintEven(int[] arr){
+    int result=0;
+    foreach(int i in arr){
+        if(i%2==0){result++;}
+    }
+    Write(result);
+}
+PrintEven(arr);
