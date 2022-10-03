efb7ac2074ecfc2d6cb90e63305b0d158320c1b1
AlekseyP <dr.Starc@ya.r>
2022-10-03 16:05:06 +0300

    оптимальное решение

--- DiagonalMultiplication/Program.cs
+++ DiagonalMultiplication/Program.cs
@@ -1,19 +1,15 @@
 ﻿using static System.Console;
 Clear();
 
-int[] DiagonalMultiplication(){
-    int[] arr={1, 2, 3, 4, 5};
-    // определяем длину получаемого массива
+int[] DiagonalMultiplication(int[]arr){
     int len = arr.Length;
     int[] result=new int[len/2+len%2];
-    for(int i=0; i<len/2; i++){
-        result[i]=arr[i]*arr[len-i-1];
-        if(arr[i]==arr[len-1-i]){break;}
-        if(result[i]==arr[len-i-1]){
-            result[arr[result.Length/2]]=arr[len/2];
+    for (int i=0; i<result.Length; i++){
+        if(i!=len-i-1){
+            result[i]=arr[i]*arr[len-i-1];
+        }else{
+            result[i]=arr[len/2];
         }
     }
     return result;
 }
-Write(String.Join(", ", DiagonalMultiplication()));
-
