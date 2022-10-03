9f8fa01c9762d1dad0e626e6f5614ee02baea49b
AlekseyP <dr.Starc@ya.r>
2022-10-03 16:35:54 +0300

    preparation))

--- dz5/Program.cs
+++ dz5/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+// масив для всех задач
+int[] arr=new int[10];
+Random rnd=new Random();
+for(int i=0; i<10; i++){
+    arr[i]=rnd.Next(99, 1000);
+}
