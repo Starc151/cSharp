b709dc39580f71298eed5b74a27b0eaeba9e53a1
Aleksey Pashkov <dr.Starc@ya.ru>
2022-10-04 23:36:47 +0300

    task41

--- dz6/Program.cs
+++ dz6/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Write("Введите ряд чисел:");
+int[] arr=Array.ConvertAll(ReadLine().Split(new char[]{' ', ',', '.'}, StringSplitOptions.RemoveEmptyEntries), int.Parse);
+int GetNumberPositive(int[] arr){
+    int count=0;
+    foreach(int i in arr){
+        count++;
+    }
+    return count;
+}
+Write($"Чисел больше 0 среди введёных: {GetNumberPositive(arr)}");
