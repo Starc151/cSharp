0c400335a39a8e4e2c5e6c48ee9f2090eb486a83
Aleksey Pashkov <dr.Starc@ya.ru>
2022-10-08 19:38:52 +0300

    task50

--- dz7/Program.cs
+++ dz7/Program.cs
@@ -3,7 +3,7 @@ Clear();
 
 //Задача 47. Задайте двумерный массив размером m×n,
 // заполненный случайными вещественными числами.
-double[,] GetMatrixArr(int rows, int colums)
+double[,] GetMatrix(int rows, int colums)
 {
     double[,] inArr = new double[rows, colums];
     Random rnd = new Random();
@@ -16,8 +16,9 @@ double[,] GetMatrixArr(int rows, int colums)
     }
     return inArr;
 }
-void PrintMatrixArray(double[,] inArr)
+void PrintMatrix(double[,] inArr)
 {
+    WriteLine($"Случайная матрица {inArr.GetLength(0)} на {inArr.GetLength(1)}:");
     for (int i = 0; i < inArr.GetLength(0); i++)
     {
         for (int j = 0; j < inArr.GetLength(1); j++)
@@ -26,9 +27,23 @@ void PrintMatrixArray(double[,] inArr)
         }
         WriteLine();
     }
+    WriteLine();
 }
-Write("Введите количество строк: ");
-int rowsInArray = Convert.ToInt32(ReadLine());
-Write("Введите количество стлбцов: ");
-int columsInArray = Convert.ToInt32(ReadLine());
-PrintMatrixArray(GetMatrixArr(rowsInArray, columsInArray));
+//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+void GetValueInMatrix(double[,] inArr, int positionRow, int positionColum)
+{
+    if (positionRow-1<inArr.GetLength(0) && positionColum-1<inArr.GetLength(1)){
+        WriteLine($"Искомый элемент по заданным значениям: {inArr[positionRow-1, positionColum-1]}");
+    } else{
+        WriteLine("Такого элемента нет");
+    }
+}
+Write("ВВедите количество строк и столбцов матрицы: ");
+int[] paramMatrix = Array.ConvertAll(ReadLine().Split(new char[]{' ', ','}, StringSplitOptions.RemoveEmptyEntries), int.Parse);
+Write("Введите искомую позицию в матрице: ");
+int[] position = Array.ConvertAll(ReadLine().Split(new char[]{' ', ','}, StringSplitOptions.RemoveEmptyEntries), int.Parse);
+
+double[,] arrForDz7=GetMatrix(paramMatrix[0], paramMatrix[1]);
+PrintMatrix(arrForDz7);
+GetValueInMatrix(arrForDz7, position[0], position[1]);
