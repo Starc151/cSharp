b3dc5227c1e8ef9bf80d047615058c7614173f47
AlekseyP <dr.Starc@ya.r>
2022-10-11 11:03:06 +0300

    array for dz8

--- dz8/Program.cs
+++ dz8/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+int[,] GetMatrix(){
+    int[,] inArr=new int [4, 4];
+    Random rnd=new Random();
+    for(int i=0; i<4; i++){
+        for(int j=0; j<4; j++){
+            inArr[i, j]=rnd.Next(0, 11);
+        }
+    }
+    return inArr;
+}
+ void PrintMatrix(int[,] inArr){
+    for(int i=0; i<inArr.GetLength(0); i++){
+        for(int j=0; j<inArr.GetLength(1); j++){
+            Write($"{inArr[i, j]} ");
+        }
+        WriteLine();
+    }
+ }
+ int[,] arrForDz8=GetMatrix();
+  PrintMatrix(arrForDz8);
